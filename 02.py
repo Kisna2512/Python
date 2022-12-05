b7c78757c465a4e50e1471cf573fe9202886990d
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-12-05 13:41:41 +0530

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,23 @@
+print("hey I'm a good boy and \nYou are also good")
+# \n- This is a Escape Sequence character in Python
+
+# "#" Chracter is used to comment in Python
+
+print("Hey I'm a \"good boy\" ")
+
+# \"\"-> This is Double quote Escape chracter
+
+
+'''This is multiline comment in Python
+99999999999999999999999999999999999
+'''
+
+print("Krishna")
+
+print("Hey", 8, 45, 5)
+
+
+# Use ca use Separator
+
+print("Hey", 5, 6, sep="~", end=" ")
+print("Krishna")
