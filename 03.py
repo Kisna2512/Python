e8e4b25ee9f9bccfbfa22ebf781e97e7959ddcc5
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-12-06 23:01:40 +0530

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,32 @@
+# variables and data types in Python
+a = 1234
+print(a)
+b = "Krishna"
+print(b)
+c = True
+print(c)
+d = None
+print(d)
+e = 1.74000
+print(e)
+print("The type of a is:", type(a))
+print(complex(8, 4))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+list = [1, 2, 3, 4]
+print(list)
+print(type(list))
+
+tuple = (1, 2, 3, 4)
+print(tuple)
+print(type(tuple))
+
+dict = {
+    "name": "Krishna",
+    "Age": 20,
+    "canVote": True}
+
+print(dict)
