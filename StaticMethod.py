182ebfce1f01a725c3f3f3e105575098564218d3
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2023-02-06 06:43:19 +0000

    Static method

---
+++ StaticMethod.py
@@ -0,0 +1,9 @@
+class Student:
+  def get_details(fname,lname):
+    print("Name ",fname,"  ","lastname ",lname)
+
+
+
+Student.get_details("Krishna","Kotgire") 
+# obj=Student()
+# obj.get_details("Krishna","Kotgire") We cannot access static method using object
