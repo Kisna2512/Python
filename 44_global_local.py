0563bbff108ff3437d3d89eeb11403f1ff94dae7
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2023-03-01 07:03:23 +0000

    If you want to print y then it will give error because it is local variable

--- 44_global_local.py
+++ 44_global_local.py
@@ -3,9 +3,11 @@ x = 4
 
 def hello():
     x = 5
+    y = 1
     print(f"The local variable x is {x}")
 
 
 print(f"The global varibale x is {x}")
 hello()
 print(f"The global varibale x is {x}")
+print(y)
